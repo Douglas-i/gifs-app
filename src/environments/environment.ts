cece2a2c889d14af5d81e0b9fc5919d7c74bca4e
douglas-i <hdouglasu@gmail.com>
2025-02-26 22:11:44 -0600

    Add gifsService and gifMapper

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,7 @@ export const environment = {
     companyName: 'Gifs',
     companyName2: 'App',
     companySlogan: 'Maneja tus gifs',
+
+    giphyApiKey: 'd1DrNmMHywzyPmtUNsIUWfQU0TVFMdJu',
+    giphyUrl: 'https://api.giphy.com/v1'
 };
